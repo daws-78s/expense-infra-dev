fa4b071b60415b94d81b05a6255f85e04201eb08
joindevops <info@joindevops.com>
2024-06-12 19:40:36 +0530

    functions

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -215,6 +215,26 @@ resource "aws_security_group_rule" "bastion_public" {
   security_group_id = module.bastion.sg_id
 }
 
+#added as part of Jenkins CICD
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.backend.sg_id
+}
+
+#added as part of Jenkins CICD
+resource "aws_security_group_rule" "frontend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.frontend.sg_id
+}
+
 # not required, we can connect from VPN
 # resource "aws_security_group_rule" "frontend_public" {
 #   type              = "ingress"
